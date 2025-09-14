25e1af9effffb6583524b68841583c23e80de856
lunarissigma <offahigh200@gmail.com>
2025-09-14 03:20:49 -0700

    init

---
+++ 13.40/framework.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <thread>
+#include <iostream>
+#include "SDK/SDK.hpp"
+#include "Minhook/MinHook.h"
+using namespace SDK;
